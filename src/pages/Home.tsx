513ad2ddcf770d2fd0fba53c57bf347c283f6e09
Nayan Bastola <nayanbastola777@gmail.com>
2023-08-16 14:40:53 -0400

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Title, Wrapper } from "./styles/homestyle";
+
+const Home = () => {
+  return (
+    <>
+      <Wrapper>
+        <Title>Hello World!</Title>
+      </Wrapper>
+    </>
+  );
+};
+
+export default Home;
